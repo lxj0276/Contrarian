1ccca78568e51f67da97bc145e2ddc66241ef8b1
Kasper Lin <kasperlxk@outlook.com>
2019-03-02 17:38:04 +0800

    Data Analysis version 2.

--- Contrarian Codes/Contrarian.py
+++ Contrarian Codes/Contrarian.py
@@ -10,6 +10,10 @@ from scipy import stats
 import itertools
 import datetime as dt 
 from dateutil.relativedelta import relativedelta
+from math import floor
+import numpy as np
+import ffn
+import matplotlib
 
 # 1 处理数据。
 
@@ -21,14 +25,27 @@ from dateutil.relativedelta import relativedelta
 # 从本地磁盘读取数据。
 class Data(object):
 
+    '''
+    param: 
+        filepath: 子目录
+    '''
+
+    # 母目录
+    path = r'C:\\Users\\KasperLin\\OneDrive - whu.edu.cn\\work\\study\\quant\\Contrarian\\data'
+
     def __init__(self, filepath):
         self.filepath = filepath
-        self.path = r'C:\\Users\\KasperLin\\OneDrive - whu.edu.cn\\work\\study\\quant\\Contrarian\\data'
 
     def get_data(self):
+
+        '''
+        return: 子目录下的文件
+        '''
+
         self.data = pd.read_csv(self.path + self.filepath)
         return self.data 
 
+# 获取数据
 # day = Data(r'\\day\\day.csv').get_data() # 日频
 # week = Data(r'\\week\\week.csv').get_data() # 周频
 month = Data(r'\\month\\month.csv').get_data() # 月频
@@ -40,6 +57,8 @@ year = Data(r'\\year\\year.csv').get_data() # 年频
 
 # 由于年度数据时间的数据类型为整数而不是字符串，统一为字符串。
 year = year.astype({'Trdynt':'str'})
+# 由于月度数据的格式无法直接进行计算，统一为datetime格式。
+month['Trdmnt'] = pd.to_datetime(month['Trdmnt'], format = '%b-%y')
 
 '''
 # 由于周频数据时间的数据有BUG，错误地将前12周显示为前12月
@@ -59,170 +78,219 @@ for i in range(len(week)):
         date = dt.datetime.strptime(date_str, '%y-%b')
         date_modified = dt.datetime.strftime(date, '%Y-%m')
         week.iloc[i, 1] = date_modified
-
-import os 
-# 尝试从本地读取数据。
-path = r'C:\\Users\\KasperLin\\OneDrive - whu.edu.cn\\work\\study\\quant\\Contrarian\\data\\week\\week.csv'
-# 由于库本身的bug，必须先用os模块切换到文件所在目录。
-pwd = os.getcwd()
-os.chdir(os.path.dirname(path))
-# 保存数据。
-week = pd.read_csv(os.path.basename(path))
-# 再切换回去原来的工作目录。
-os.chdir(pwd)
 '''
 
 
 ## 1.2 输出数据
 
-# * J、K分别代表排序期、持有期。
-# 获取最终结果的函数格式为：
-# 频率：年、月、周、日分别用'y'、'm'、'w'、'd'代替（都是字符串）
-# 赢家组合：“Return(频率).hold_winner_return(基准期，排序期, 持有期)”
-# 输家组合：输家组合：“Return(频率).hold_loser_return(基准期，排序期, 持有期)”
-# * 年回报率采用CSMAR数据库中的“考虑现金红利再投资的年个股回报率”。
-class Return(object):
+class Rank(object):
 
-    def __init__(self, t):
+    '''
+    param:
+        t: 频率，年-'y'，月-'m'，周-'w'，日-'d'。(str)
+    '''
+
+    def __init__(self, t = 'm'):
         if t == 'y':
             self.t = 'y'
             self.data = year
             self.time_label = 'Trdynt'
-            self.return_label = 'Yretwd'
-            self.return_column = 7
-            self.open_label = 'Yopnprc'
-            self.close_label = 'Yclsprc'
-            self.name = 'year'
+            self.return_label = 'Yretnd'
+            self.value_label = 'Ysmvttl'
         elif t == 'm':
             self.t = 'm'
             self.data = month
             self.time_label = 'Trdmnt'
             self.return_label = 'Mretwd'
-            self.return_column = 7
-            self.open_label = 'Mopnprc'
-            self.close_label = 'Mclsprc'
-            self.name = 'month'
-        elif t == 'w':
-            self.t = 'w'
-            self.data = week
-            self.time_label = 'Trdwnt'
-            self.return_label = 'Wretwd'
-            self.return_column = 7
-            self.open_label = 'Wopnprc'
-            self.close_label = 'Wclsprc'
-            self.name = 'week'
-        elif t == 'd':
-            self.t = 'd'
-            self.data = day
-            self.time_label = 'Trddnt'
-            self.return_label = 'Dretwd'
-            self.return_column = 7
-            self.open_label = 'Dopnprc'
-            self.close_label = 'Dclsprc'
-            self.name = 'day'
-
-    def get_list(self):
-        time_list = self.data[self.time_label].drop_duplicates()
-        return time_list
-
-    # 定义函数get_data(基准期)获取第N期（包含第N期）的按照收益率从大到小排序的数据。
-    # 剔除样本缺失的数据。
-    def get_data(self, n):
-        data = self.data[self.data[self.time_label] == n].dropna()\
-        .sort_values(by = self.return_label, ascending = False)
-        return data
+            self.value_label = 'Msmvttl'
 
     def timedelta(self, n, delta):
+
+        '''
+        param:
+            n: 日期，年-'YYYY'，月-'YYYY-MM'。(str)
+            delta: 日期变动值，为正则向前变动，为负反之。(int)
+        return:
+            变动后的日期值。(str)
+        '''
+
+        # 先把字符串格式的日期转换为可计算的日期，计算后再转换回字符串。
+        
         if self.t == 'y':
             tn = (dt.datetime.strptime(n, '%Y') + \
             relativedelta(years = delta)).strftime('%Y')
+
         elif self.t == 'm':
-            tn = (dt.datetime.strptime(n, '%b-%y') + \
-            relativedelta(months = delta)).strftime('%b-%y')
+            tn = (dt.datetime.strptime(n, '%Y-%m') + \
+            relativedelta(months = delta)).strftime('%Y-%m')
+
         return tn
 
-    # 定义函数sort(基准期，排序期)获取第N期按照前J期排序得到的收益率数据。
-    def sort(self, n, J):
+    def rank(self, n, J):
 
-        data = Return(self.t).get_data(n)
-        return_list = [] # 空列表保存平均收益率数据
+        '''
+        param:
+            n: 日期，年-'YYYY'，月-'YYYY-MM'。(str)
+            J: 排序期。(int)
+        return:
+            按照排序期股票累计收益率降序排序的股票代码及其收益率列表。(pd.Series)
+        '''
 
-        # 取过去J期的股票数据的交集。
-        past_data = Return(self.t).get_data(Return(self.t).timedelta(n, -J))
-        data = data[data['Stkcd'].isin(past_data['Stkcd'])]
-
-        # 遍历data的i个股票数据到过去J期的数据中取得过去的收益率。
-        for i in range(len(data)):
-
-            code = data.iloc[i, 0] # 基准期的第i个股票代码
-
-            base_price = Return(self.t).get_data(n)[self.close_label].iloc[i]
-            past_data = Return(self.t).get_data(Return(self.t).timedelta(n, -J)) # 第J期的数据
-            past_price = past_data[past_data['Stkcd'] == code][self.open_label].iloc[0] # 第i个股票第J期的历史收益率
-            cum_return = (base_price - past_price) / past_price
-
-            return_list.append(cum_return) # 在列表加上平均收益率
+        start = Rank(self.t).timedelta(n, -J)
+        end = Rank(self.t).timedelta(n, 1)
         
-        data.loc[:, 'past_ret'] = return_list
-        data = data.sort_values(by = "past_ret", ascending = False)
+        # 取出排名期内的数据。
+        rank = self.data[(self.data[self.time_label] > start) & (self.data[self.time_label] < end)]
 
-        return data
+        # 计算每只股票在排名期的累计收益率。
+        aggret = rank.groupby('Stkcd')[self.return_label].agg('sum')
 
-    # 定义函数get_winner(基准期，排序期)获取排序期赢家组合的数据。
-    def get_winner(self, n, J):
-        data = Return(self.t).sort(n, J)
-        return data.head(int(len(data) / 5))
+        # 将累计收益率排序。
+        aggret.sort_values(inplace = True)
 
-    # 定义函数winner_return(基准期，排序期)获取排序期赢家组合的收益率。
-    def winner_return(self, n, J):
-        return Return(self.t).get_winner(n, J)[self.return_label].mean()
+        return aggret
 
-    # 定义函数get_loser(基准期，排序期)获取排序期输家组合的数据。
-    def get_loser(self, n, J):
-        data = Return(self.t).sort(n, J)
-        return data.tail(int(len(data) / 5))
+    def get_port(self, n, J, percentage = 0.2):
 
-    # 定义函数loser_return(基准期，排序期)获取排序期输家组合的收益率。
-    def loser_return(self, n, J):
-        return Return(self.t).get_loser(n, J)[self.return_label].mean()
+        '''
+        param:
+            n: 日期，年-'YYYY'，月-'YYYY-MM'。(str)
+            J: 排序期。(int)
+        param（可选）:
+            percentage: 分层比例。(float)
+        return:
+            赢家股票列表。(pe.Series)
+            输家股票列表。(pe.Series)
+        '''
 
-    # 定义函数hold(基准期，排序期，持有期，)
-    def hold(self, n, J, K, port):
+        # 获取排序期股票数据。
+        rank = Rank(self.t).rank(n, J)
 
-        return_list = [] # 空列表保存平均收益率数据
+        length = floor(len(rank) * percentage)
 
-        # 清洗数据
-        future_data = Return(self.t).get_data(Return(self.t).timedelta(n, +K))
-        port = port[port['Stkcd'].isin(future_data['Stkcd'])] 
+        # 取收益率最高的一层为赢家。
+        winner_list = rank.index[-length:]
+        # 取收益率最低的一层为输家。
+        loser_list = rank.index[0:length]
 
-        for i in range(len(port)):
+        return winner_list, loser_list
+    
+    def hold(self, n, J, K, percentage = 0.2, large = False, small = False, value_weighted = False):
 
-            code = port.iloc[i, 0]
-            base_price = Return(self.t).get_data(Return(self.t).timedelta(n, +1))[self.open_label].iloc[i]
-            future_data = Return(self.t).get_data(Return(self.t).timedelta(n, +K))
-            future_price = future_data[future_data['Stkcd'] == code]\
-            [self.close_label].iloc[0]
-            cum_return = (future_price - base_price) / base_price
+        '''
+        param:
+            n: 日期，年-'YYYY'，月-'YYYY-MM'。(str)
+            J: 排序期。(int)
+            K: 持有期。(int)
+        param（可选）:
+            percentage: 分层比例，默认为0.2。(float)
+            large: 是否只交易市值最大的前N个股票，默认为否(False)。(int / Bool)
+            small: 是否只交易市值最小的前N个股票，默认为否(False)。(int / Bool)
+            value_weighted: 是否市值加权构建投资组合，默认为否(False)。(Bool)
+        return:
+            赢家收益率。(float)
+            输家收益率。(float)
+        '''
 
-            return_list.append(cum_return / K)
+        start = Rank(self.t).timedelta(n, 0)
+        end = Rank(self.t).timedelta(n, K+1)
+        observe = self.data[self.data[self.time_label] == n]
 
-        port.loc[:, 'hold_ret'] = return_list
-        port = port.sort_values(by = 'hold_ret', ascending = False)
+        if (large == False) | (small == False):
+            port = Rank(self.t).get_port(n, J, percentage = percentage)
+            winner_list = list(port[0])
+            loser_list = list(port[1])
 
-        return port
+        if large != False:
+            port = Rank(self.t).get_port(n, J, percentage = percentage)
+            large = list(observe.sort_values(by = self.value_label).tail(large)['Stkcd'])
+            winner_list = set(large).intersection(list(port[0]))
+            loser_list = set(large).intersection(list(port[1]))
 
-    def hold_winner(self, n, J, K):
-        data = Return(self.t).hold(n, J, K, Return(self.t).get_winner(n, J))
-        return data
+        if small != False:
+            port = Rank(self.t).get_port(n, J, percentage = percentage)
+            small = list(observe.sort_values(by = self.value_label).head(small)['Stkcd'])
+            winner_list = set(small).intersection(list(port[0]))
+            loser_list = set(small).intersection(list(port[1]))
+        
+        # 取出持有期内的数据。
+        hold = self.data[(self.data[self.time_label] > start) & (self.data[self.time_label] < end)]
+        hold_winner = hold[hold['Stkcd'].isin(winner_list)]
+        hold_loser = hold[hold['Stkcd'].isin(loser_list)]
 
-    def hold_loser(self, n, J, K):
-        return Return(self.t).hold(n, J, K, Return(self.t).get_loser(n, J))
+        # 计算每只股票在持有期的累计收益率。
+        aggret_winner = hold_winner.groupby('Stkcd')[self.return_label].sum()
+        aggret_loser = hold_loser.groupby('Stkcd')[self.return_label].sum()
 
-    def hold_winner_return(self, n, J, K):
-        return Return(self.t).hold_winner(n, J, K)['hold_ret'].mean()
+        if value_weighted == True:
+            # 市值加权构建投资组合计算收益率
+            winner = pd.DataFrame(aggret_winner)
+            observe_winner = observe[observe['Stkcd'].isin(list(aggret_winner.index))]
+            winner['Value'] = list(observe_winner[self.value_label])
+            winner_ret = np.average(winner[self.return_label], weights = winner['Value'])
 
-    def hold_loser_return(self, n, J, K):
-        return Return(self.t).hold_loser(n, J, K)['hold_ret'].mean()    
+            loser = pd.DataFrame(aggret_loser)
+            observe_loser = observe[observe['Stkcd'].isin(list(aggret_loser.index))]
+            loser['Value'] = list(observe_loser[self.value_label])
+            loser_ret = np.average(loser[self.return_label], weights = loser['Value'])
+
+        if value_weighted == False:
+            winner_ret = sum(aggret_winner) / len(aggret_winner)
+            loser_ret = sum(aggret_loser) / len(aggret_loser)
+
+        return winner_ret, loser_ret
+
+    def perf(self, start, n_month, J, K, strategy = 'wl', percentage = 0.2, large = False, small = False, value_weighted = False):
+
+        table = pd.DataFrame(columns = ['Return', 'Equity'])
+
+        time_list = []
+        return_list = []
+        
+        for i in range(n_month - K):
+
+            time_list.append(Rank(self.t).timedelta(start, i))
+
+            w = Rank(self.t).hold(
+                n = Rank(self.t).timedelta(start, i), 
+                J = J, 
+                K = K, 
+                percentage = percentage, 
+                large = large, 
+                small = small, 
+                value_weighted = value_weighted
+            )[0]
+
+            l = Rank(self.t).hold(
+                n = Rank(self.t).timedelta(start, i), 
+                J = J, 
+                K = K, 
+                percentage = percentage, 
+                large = large, 
+                small = small, 
+                value_weighted = value_weighted
+            )[1]
+
+            if strategy == 'wl':
+                ret = (w - l)*100
+            elif strategy == 'w':
+                ret = w*100
+            elif strategy == 'l':
+                ret = l*100
+
+            return_list.append(ret)
+
+        table['Time'] = time_list
+        table['Time'] = pd.to_datetime(table['Time'], format = '%Y-%m')
+        table.set_index('Time', inplace = True)
+        table['Return'] = return_list
+        table.iloc[0, 0] = 0
+        table['Equity'] = table['Return'].cumsum() + 100
+        table.iloc[0, 1] = 100
+
+        perf = table['Equity'].calc_stats()
+
+        return perf
 
 ### 1.1.1 年频
 
@@ -230,34 +298,31 @@ class Return(object):
 
 # > 使用Jegadeesh和Titman(1993)的方法（重叠法）进行收益率检验。
 
-# 列表总结结果并检验。
-# 获得最终检验统计表格的函数格式为：
-# “Test(频率, 开始期, 结束期, 排序期持有期样本选择跨度)”
-class Test(object):
+class Test(Rank):
 
-    def __init__(self, t, start, end, freq, n_sample = None):
+    '''
+    param:
+        start: 开始时间，年-'YYYY'、月-'YYYY-MM'。(str)
+        end: 结束时间，年-'YYYY'、月-'YYYY-MM'。(str)
+        t: 频率，年-'y'，月-'m'，周-'w'，日-'d'。(str)
+        freq: 排序持有期观察频率。(int)
+        n_sample: 排序持有期观察次数。(int)
+    '''
+
+    def __init__(self, start, end, freq, t = 'm', n_sample = None):
 
         if t == 'y':
             self.t = 'y'
-            start_date = dt.datetime.strptime(start, '%Y')
-            end_date = dt.datetime.strptime(end, '%Y')
-            self.length = relativedelta(end_date, start_date).years + 1
+            start_date = int(start)
+            end_date = int(end)
+            self.length = end_date - start_date + 1
 
         if t == 'm':
             self.t = 'm'
-            start_date = dt.datetime.strptime(start, '%b-%y')
-            end_date = dt.datetime.strptime(end, '%b-%y')
+            start_date = dt.datetime.strptime(start, '%Y-%m')
+            end_date = dt.datetime.strptime(end, '%Y-%m')
             self.length = relativedelta(end_date, start_date).years * 12 + \
             relativedelta(end_date, start_date).months + 1
-
-        if t == 'w':
-            self.t = 'w'
-            start_date = dt.datetime.strptime(start, '%Y-%W')
-            end_date = dt.datetime.strptime(end, '%Y-%W')
-            self.length = (relativedelta(end_date, start_date).years * 365 + \
-            relativedelta(end_date, start_date).months * 30 + \
-            relativedelta(end_date, start_date).days) / 7
-
 
         self.start = start
         self.end = end
@@ -266,52 +331,95 @@ class Test(object):
         if freq == 1:
             self.axis = list(range(1, self.length + 1))[:n_sample]
         else:
-            self.axis = [1] + list(range(self.freq, self.freq * int(self.length / self.freq) + 1, self.freq))[:n_sample]
+            self.axis = [1] + list(range(self.freq, self.freq * (self.length // self.freq) + 1, self.freq))[:(n_sample-1)]
 
-    def table(self, strategy):
+    def table(
+        self, 
+        strategy = 'wl', 
+        percentage = 0.2, 
+        large = False, 
+        small = False, 
+        value_weighted = False):
 
+        '''
+        param（可选）:
+            strategy: 交易策略，“赢家-输家”-'wl'、“单边输家”-'l'、“单边赢家”-'w'，默认为“赢家-输家”('wl')。(str)
+            large: 是否只交易市值最大的前N个股票，默认为否(False)。(int / Bool)
+            small: 是否只交易市值最小的前N个股票，默认为否(False)。(int / Bool)
+            value_weighted: 是否市值加权构建投资组合，默认为否(False)。(Bool)
+        return:
+            最终表格。(pd.DataFrame)
+        '''
+        
         table = pd.DataFrame(index = self.axis, columns = self.axis)
 
         for j in self.axis:
             for k in self.axis:
-                # 赢家 - 输家组合
-                base = list(range(j, self.length - k + 1)) # 基准年列表
+                
+                base = list(range(j, self.length - k + 1, k)) # 基准列表
 
-                if base == []:
-                    table.loc[j, k] = ''
-
-                elif (j + k) > self.length:
+                if (base == []) & ((j + k) > self.length):
                     table.loc[j, k] = ''
 
                 elif (j + k) <= self.length:
+
                     return_list = []
+                    
                     for n in base:
+
+                        # 定义引用方法。
+                        ret = Rank(self.t).hold(
+                            n = Rank(self.t).timedelta(self.start, +(n-1)), 
+                            J = j, 
+                            K = k, 
+                            percentage = percentage, 
+                            large = large, 
+                            small = small, 
+                            value_weighted = value_weighted)
+
                         if strategy == 'wl':
-                            return_result = \
-                            Return(self.t).hold_winner_return(Return(self.t).timedelta(self.start, +(n-1)), j, k) - \
-                            Return(self.t).hold_loser_return(Return(self.t).timedelta(self.start, +(n-1)), j, k)
-                        if strategy == 'l':
-                            return_result = Return(self.t).hold_loser_return(Return(self.t).timedelta(self.start, +(n-1)), j, k)
+                            return_result = ret[0] - ret[1]
+
+                        elif strategy == 'l':
+                            return_result = ret[1]
+
+                        elif strategy == 'w':
+                            return_result = ret[0]
+
                         return_list.append(return_result)
-                    sum_return = 0
-                    for i in return_list:
-                        sum_return += i
-                    avg_return = sum_return / len(base)
+
+                    avg_return = sum(return_list) / len(return_list)
                     zero_list = list(itertools.repeat(0, len(return_list)))
-                
+
+                    if self.t == 'y':
+                        ret = str(round(avg_return * 100 / k, 2))
+                    if self.t == 'm':
+                        ret = str(round(avg_return * 100 * 12 / k, 2))
+
                     if len(return_list) == 1:
-                        table.loc[j, k] = str(round(avg_return, 4))
+                        table.loc[j, k] = "%s" % ret + "%"
                     else:
                         ttest = stats.ttest_ind(return_list, zero_list)[0]
-                        if type(ttest) == str:
-                            table.loc[j, k] = str(round(avg_return, 4))
-                        else:
-                            table.loc[j, k] = str(round(avg_return, 4)) + "(%s)" % round(ttest, 4)
+                        table.loc[j, k] = "%s" % ret + "%" + "(%s)" % round(ttest, 2)
         
         return table
 
 # ## 2.1 年频
 # ## 2.2 月频
+'''
+month_sample = Test(
+    start = '2008-01', 
+    end = '2017-12', 
+    t = 'm', 
+    freq = 3, 
+    n_sample = 5
+    ).table(
+        strategy = 'l', 
+        large = False, 
+        small = 200, 
+        value_weighted = False
+    )
+'''
 # ## 2.3 周频
 # ## 2.4 日频
 
